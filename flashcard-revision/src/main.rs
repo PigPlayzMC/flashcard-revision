ae572a290e73c2cbe3041962aedb56202d97aa1b
PigPlayzMC <charliehbird@gmail.com>
2024-12-06 16:06:21 +0000

    Flashcards can be added to weak_flashcards

--- flashcard-revision/src/main.rs
+++ flashcard-revision/src/main.rs
@@ -1,26 +1,49 @@
-fn add_new_flashcard() {
+// All flashcards follow this structure
+pub struct Flashcard {
+    subject: i32, // Which subject these fall into
+    last_accessed: i32, //Change when date storage established
+    question: String, // Question or front text of the card
+    answer: String, // Answer or back text of the card
+    correct: i32, // Times the user has gotten the question correct
+    incorrect: i32, // Times the user has gotten the question incorrect
+}
+
+fn add_new_flashcard(sub:i32, ques: String, ans: String) -> Flashcard{
     // This takes inputs and turns it into a struct
     // Then returns it.
+    return Flashcard {
+        subject: sub,
+        last_accessed: 0, // Doesn't need to be set as it has never been accessed
+        question: ques,
+        answer: ans,
+        correct: 0, // Never answered
+        incorrect: 0, // Never answered
+    }
+}
+
+fn get_random_flashcard(card_set:Vec<Flashcard>) {
+    
 }
 
 fn main() {
     // Declare subjects to 'store' flashcards in
-    let mut subjects = Vec::new(); // Store flashcards
+    let mut subjects: Vec<String> = Vec::new(); // Store flashcards
 
     // Declare flashcard variables
-    let mut strong_flashcards = Vec::new(); // Flashcards done well generally
-    let mut learning_flashcard = Vec::new(); // Flashcards done well sometimes
-    let mut weak_flashcards = Vec::new(); // Flashcards done poorly
+    let mut strong_flashcards: Vec<Flashcard> = Vec::new(); // Flashcards done well generally
+    let mut learning_flashcard: Vec<Flashcard> = Vec::new(); // Flashcards done well sometimes
+    let mut weak_flashcards: Vec<Flashcard> = Vec::new(); // Flashcards done poorly
 
-    // All flashcards follow this structure
-    pub struct Flashcard {
-        catagory: i32, // Which subject these fall into
-        last_accessed: i32, //Change when date storage established
-        question: String, // Question or front text of the card
-        answer: String, // Answer or back text of the card
-        correct: i32, // Times the user has gotten the question correct
-        incorrect: i32, // Times the user has gotten the question incorrect
-    }
+    
 
-    strong_flashcards.push(flashcard);
+    // This is the current flashcard number which determines which flashcard is used.
+    let mut flashcard: i32 = 0;
+
+    // Add a flashcard to a subject
+    let sub = 0;
+    let ques: String = "Hello World!".to_owned();
+    let ans: String = "Hello World!".to_owned();
+
+    // Add a new flashcard
+    weak_flashcards.push(add_new_flashcard(sub, ques, ans));
 }
