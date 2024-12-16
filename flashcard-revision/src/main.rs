575d5137cd54415ced19e6930b01006d175cd5c6
PigPlayzMC <charliehbird@gmail.com>
2024-12-16 14:46:23 +0000

    Attempt to make multiple questions be asked by each flashcard

--- flashcard-revision/src/main.rs
+++ flashcard-revision/src/main.rs
@@ -5,7 +5,6 @@ use chrono::{self, Local, NaiveDate};
 // All flashcards follow this structure
 #[derive(Clone)]
 pub struct Flashcard {
-	subject: i32, // Which subject these fall into
 	last_accessed: NaiveDate, // Change when date storage established
 	question: String, // Question or front text of the card
 	answer: String, // Answer or back text of the card
@@ -13,11 +12,10 @@ pub struct Flashcard {
 	incorrect: i32, // Times the user has gotten the question incorrect
 }
 
-fn add_new_flashcard(sub:i32, ques: String, ans: String) -> Flashcard{
+fn add_new_flashcard(ques: String, ans: String) -> Flashcard {
 	// This takes inputs and turns it into a struct
 	// Then returns it.
 	return Flashcard {
-		subject: sub,
 		last_accessed: Local::now().date_naive(), // Doesn't need to be set as it has never been accessed
 		question: ques,
 		answer: ans,
@@ -26,11 +24,17 @@ fn add_new_flashcard(sub:i32, ques: String, ans: String) -> Flashcard{
 	}
 }
 
-fn get_random_flashcard<'a>(card_set: &'a mut Vec<Flashcard>) -> usize {
+fn get_random_flashcard<'a>(card_set: &'a mut Vec<Flashcard>, previous_indexes: Vec<usize>, length: usize) -> usize {
 	let mut rng = rand::thread_rng();
-	let length = card_set.len();
-	let rand_number = rng.gen_range(0..length);
-	return rand_number; // Return random value
+	println!("Randomising card from 0 to {length}");
+	loop {
+		let rand_number = rng.gen_range(0..length);
+		if previous_indexes.contains(&rand_number) {
+			// Flash card already done. Retry!
+		} else {
+			return rand_number; // Return random value
+		}
+	}
 }
 
 fn congratulations(flashcard: Flashcard) {
@@ -44,7 +48,7 @@ fn commiserations(flashcard: Flashcard) {
 }
 
 fn revision_summary(correct_total : i32, cards_practiced : i32,
-	weak_flashcards : Vec<Flashcard>, learning_flashcards : Vec<Flashcard>, strong_flashcards : Vec<Flashcard>) {
+weak_flashcards : Vec<Flashcard>, learning_flashcards : Vec<Flashcard>, strong_flashcards : Vec<Flashcard>) {
 	println!("Post flashcard breakdown:");
 	let percent_accuracy = correct_total/cards_practiced*100;
 	let mut cards= "card";
@@ -92,65 +96,56 @@ fn main() {
 	let mut weak_flashcards: Vec<Flashcard> = Vec::new(); // Flashcards done poorly
 
 	// ## Add a flashcard to a subject ##
-	let sub = 0;
 	let ques: String = "Hello World!".to_owned();
 	let ans: String = "Hello World!".to_owned();
 	// Add a new flashcard
-	weak_flashcards.push(add_new_flashcard(sub, ques, ans));
+	weak_flashcards.push(add_new_flashcard(ques, ans));
+	// ## Add a flashcard to a subject ##
+	let ques: String = "Second!".to_owned();
+	let ans: String = "Second!".to_owned();
+	// Add a new flashcard
+	weak_flashcards.push(add_new_flashcard(ques, ans));
 
 	// Per practice variables
-	let mut cards_practiced = 0;
+	let mut cards_practiced: i32 = 0;
+	let mut cards_usize: usize = 0;
 	let mut correct_total = 0;
+	let mut indexes: Vec<usize> = Vec::new();
 
-	// ## Ask a random flashcard question ##
-	// Get rand question
-	let index_of_question = get_random_flashcard(&mut weak_flashcards);
-	let mut question_to_ask = weak_flashcards[index_of_question].clone();
+
+	// ### ### REVISION LOOP ### ###
 	let practice_set = "weak"; // Dependent on wether weak, mid, or strong being practiced.
-	// Display question
-	println!("{}",question_to_ask.question);
+	let length: usize = weak_flashcards.len();
+	while cards_usize <= weak_flashcards.len() {
+		// ## Ask a random flashcard question ##
+		// Get rand question
+		let index_of_question = get_random_flashcard(&mut weak_flashcards, indexes.clone(), length);
+		indexes.push(index_of_question);
+		let mut question_to_ask = weak_flashcards[index_of_question].clone();
 
-	println!();
+		// Display question
+		println!("{}",question_to_ask.question);
 
-	// Both lines required for input handling
-	let mut input = String::new();
-	let _n = io::stdin().read_line(&mut input).unwrap();
-
-	println!();
-
-	// Compare actual answer and the input
-	println!("Your answer: {}", input.trim());
-	println!("Actual answer: {}", question_to_ask.answer);
-
-	question_to_ask.last_accessed = Local::now().date_naive(); // EVERYTHING will be a single line!
-
-	cards_practiced += 1;
-	if input.trim().to_lowercase() == question_to_ask.answer.to_lowercase() {
-		println!("Correct!");
 		println!();
-		question_to_ask.correct += 1;
 
-		if practice_set == "weak" {
-			//println!("Moving to learning...");
-			weak_flashcards.swap_remove(index_of_question);
-			learning_flashcards.push(question_to_ask.clone());
-		} else if practice_set == "learning" {
-			//println!("Moving to strong...");
-			learning_flashcards.swap_remove(index_of_question);
-			strong_flashcards.push(question_to_ask.clone());
-		}
-
-		congratulations(question_to_ask);
-		correct_total += 1;
-	} else {
-		println!("Was your answer correct? (y/n)");
+		// Both lines required for input handling
 		let mut input = String::new();
 		let _n = io::stdin().read_line(&mut input).unwrap();
+
 		println!();
-		//println!("Input: {}", input.trim().to_lowercase());
-		if input.trim().to_lowercase() == "y" { // Answer correct
+
+		// Compare actual answer and the input
+		println!("Your answer: {}", input.trim());
+		println!("Actual answer: {}", question_to_ask.answer);
+
+		question_to_ask.last_accessed = Local::now().date_naive(); // EVERYTHING will be a single line!
+
+		cards_practiced += 1;
+		if input.trim().to_lowercase() == question_to_ask.answer.to_lowercase() {
+			println!("Correct!");
+			println!();
 			question_to_ask.correct += 1;
-			
+
 			if practice_set == "weak" {
 				//println!("Moving to learning...");
 				weak_flashcards.swap_remove(index_of_question);
@@ -163,21 +158,44 @@ fn main() {
 
 			congratulations(question_to_ask);
 			correct_total += 1;
-		} else if input.trim().to_lowercase() == "n" { // Answer correct
-			question_to_ask.incorrect += 1;
+		} else {
+			println!("Was your answer correct? (y/n)");
+			let mut input = String::new();
+			let _n = io::stdin().read_line(&mut input).unwrap();
+			println!();
+			//println!("Input: {}", input.trim().to_lowercase());
+			if input.trim().to_lowercase() == "y" { // Answer correct
+				question_to_ask.correct += 1;
+				
+				if practice_set == "weak" {
+					//println!("Moving to learning...");
+					weak_flashcards.swap_remove(index_of_question);
+					learning_flashcards.push(question_to_ask.clone());
+				} else if practice_set == "learning" {
+					//println!("Moving to strong...");
+					learning_flashcards.swap_remove(index_of_question);
+					strong_flashcards.push(question_to_ask.clone());
+				}
 
-			if practice_set == "learning" {
-				//println!("Moving to weak...");
-				learning_flashcards.swap_remove(index_of_question);
-				weak_flashcards.push(question_to_ask.clone());
-			} else if practice_set == "strong" {
-				//println!("Moving to learning...");
-				strong_flashcards.swap_remove(index_of_question);
-				weak_flashcards.push(question_to_ask.clone());
+				congratulations(question_to_ask);
+				correct_total += 1;
+			} else if input.trim().to_lowercase() == "n" { // Answer correct
+				question_to_ask.incorrect += 1;
+
+				if practice_set == "learning" {
+					//println!("Moving to weak...");
+					learning_flashcards.swap_remove(index_of_question);
+					weak_flashcards.push(question_to_ask.clone());
+				} else if practice_set == "strong" {
+					//println!("Moving to learning...");
+					strong_flashcards.swap_remove(index_of_question);
+					weak_flashcards.push(question_to_ask.clone());
+				}
+
+				commiserations(question_to_ask);
 			}
-
-			commiserations(question_to_ask);
 		}
+		cards_usize += 1;
 	}
 
 	// Post revision summary
